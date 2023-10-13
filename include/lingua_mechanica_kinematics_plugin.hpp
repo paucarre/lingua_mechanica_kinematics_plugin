f0a081d6f731847a88d0ad97eef5f4d46e0d88b8
Pau Carre Cardona <pau.carre@gmail.com>
2023-10-13 16:37:11 +0000

    dev

--- include/lingua_mechanica_kinematics_plugin.hpp
+++ include/lingua_mechanica_kinematics_plugin.hpp
@@ -1,9 +1,27 @@
 #ifndef LINGUA_MECHANICA_KINEMATICS_PLUGIN_
 #define LINGUA_MECHANICA_KINEMATICS_PLUGIN_
 
+// ROS
+#include <ros/ros.h>
+
+// System
+#include <memory>
+
+// ROS msgs
+#include <geometry_msgs/Pose.h>
+#include <moveit_msgs/KinematicSolverInfo.h>
+#include <moveit_msgs/MoveItErrorCodes.h>
+
+// MoveIt
+#include <moveit/kinematics_base/kinematics_base.h>
+#include <moveit/robot_state/robot_state.h>
+
+
 #include <moveit/kinematics_base/kinematics_base.h>
 #include <kdl/chain.hpp>
 #include <kdl/jntarray.hpp>
+#include <moveit/robot_model/robot_model.h>
+#include <moveit_msgs/KinematicSolverInfo.h>
 
 namespace lingua_mechanica_kinematics_plugin
 {
@@ -136,6 +154,32 @@ public:
                         const kinematics::KinematicsQueryOptions &options) const;
 
 
+
+  /**
+   * @brief Given the desired poses of all end-effectors, compute joint angles that are able to reach it.
+   *
+   * The default implementation returns only one solution and so its result is equivalent to calling
+   * 'getPositionIK(...)' with a zero initialized seed.
+   *
+   * Some planners (e.g. IKFast) support getting multiple joint solutions for a single pose.
+   * This can be enabled using the |DiscretizationMethods| enum and choosing an option that is not |NO_DISCRETIZATION|.
+   *
+   * @param ik_poses  The desired pose of each tip link
+   * @param ik_seed_state an initial guess solution for the inverse kinematics
+   * @param solutions A vector of valid joint vectors. This return has two variant behaviors:
+   *                  1) Return a joint solution for every input |ik_poses|, e.g. multi-arm support
+   *                  2) Return multiple joint solutions for a single |ik_poses| input, e.g. underconstrained IK
+   *                  TODO(dave): This dual behavior is confusing and should be changed in a future refactor of this API
+   * @param result A struct that reports the results of the query
+   * @param options An option struct which contains the type of redundancy discretization used. This default
+   *                implementation only supports the KinematicSearches::NO_DISCRETIZATION method; requesting any
+   *                other will result in failure.
+   * @return True if a valid set of solutions was found, false otherwise.
+   */
+  bool getPositionIK(const std::vector<geometry_msgs::Pose>& ik_poses, const std::vector<double>& ik_seed_state,
+                             std::vector<std::vector<double> >& solutions, kinematics::KinematicsResult& result,
+                             const kinematics::KinematicsQueryOptions& options) const;
+
   /**
    * @brief Given a set of joint angles and a set of links, compute their pose
    *
@@ -152,15 +196,26 @@ public:
                      std::vector<geometry_msgs::Pose> &poses) const;
 
 
-  bool initialize(const std::string &robot_description,
+  bool initialize(const moveit::core::RobotModel& robot_description,
                   const std::string& group_name,
                   const std::string& base_name,
                   const std::string& tip_name,
                   double search_discretization);
 
 private:
+  moveit_msgs::KinematicSolverInfo ik_group_info_; /** Stores information for the inverse kinematics solver */
 
   int getKDLSegmentIndex(const std::string &name) const;
+  
+  std::shared_ptr<ros::ServiceClient> ik_service_client_;
+
+  unsigned int dimension_; /** Dimension of the group */
+
+  const moveit::core::JointModelGroup* joint_model_group_;
+
+  moveit::core::RobotStatePtr robot_state_;
+
+  int num_possible_redundant_joints_;
 
 }; // end class
 }
--- src/lingua_mechanica_kinematics_plugin.cpp
+++ src/lingua_mechanica_kinematics_plugin.cpp
@@ -6,57 +6,45 @@
 #include <kdl/tree.hpp>
 #include <kdl_parser/kdl_parser.hpp>
 #include <lingua_mechanica_kinematics_plugin.hpp>
+#include <lingua_mechanica_kinematics_msgs/GetPositionIK.h>
 #include <limits>
 #include <kdl/chainfksolverpos_recursive.hpp>
-#include <torch/script.h>
+
+#include <moveit_msgs/GetPositionIK.h>
+#include <class_loader/class_loader.hpp>
+#include <moveit/robot_state/conversions.h>
+
+// Eigen
+#include <Eigen/Core>
+#include <Eigen/Geometry>
 
 namespace lingua_mechanica_kinematics_plugin
 {
 
-bool LinguaMechanicaKinematicsPlugin::initialize(const std::string &robot_description,
+bool LinguaMechanicaKinematicsPlugin::initialize(const moveit::core::RobotModel& robot_model,
     const std::string& group_name,
-    const std::string& base_name,
-    const std::string& tip_name,
+    const std::string& base_frame,
+    const std::string& tip_frame,
     double search_discretization)
 {
+  
+  std::vector<std::string> tip_frames = {tip_frame};
+  storeValues(robot_model, group_name, base_frame, tip_frames, search_discretization);
 
-  ros::ServiceClient client = n.serviceClient<beginner_tutorials::AddTwoInts>("lingua_mechanica");
+  ROS_DEBUG_STREAM_NAMED("LinguaMechanicaKinematicsPlugin", "Reading joints and links from URDF");
 
-  std::vector<std::string> tip_names = {tip_name};
-  setValues(robot_description, group_name, base_name, tip_names, search_discretization);
-
-  ros::NodeHandle node_handle("~");
-
-  urdf::Model robot_model;
-  std::string xml_string;
-
-  std::string urdf_xml, full_urdf_xml;
-  node_handle.param("urdf_xml", urdf_xml, robot_description);
-  node_handle.searchParam(urdf_xml, full_urdf_xml);
-
-  ROS_DEBUG_NAMED("lingua mechanica", "Reading xml file from parameter server");
-  if (!node_handle.getParam(full_urdf_xml, xml_string))
-  {
-    ROS_FATAL_NAMED("lingua mechanica", "Could not load the xml from parameter server: %s", urdf_xml.c_str());
-    return false;
-  }
-
-  node_handle.param(full_urdf_xml, xml_string, std::string());
-  robot_model.initString(xml_string);
-
-  ROS_DEBUG_STREAM_NAMED("lingua mechanica", "Reading joints and links from URDF");
 
   KDL::Tree tree;
-
-  if (!kdl_parser::treeFromUrdfModel(robot_model, tree))
+  auto urdf = robot_model_->getURDF();
+  if (!kdl_parser::treeFromUrdfModel(*urdf, tree))
   {
     ROS_FATAL("Failed to extract kdl tree from xml robot description");
     return false;
   }
 
-  if (!tree.getChain(base_name, tip_name, chain))
+  if (!tree.getChain(base_frame, tip_frame, chain))
   {
-    ROS_FATAL("Couldn't find chain %s to %s", base_name.c_str(), tip_name.c_str());
+    ROS_FATAL("Couldn't find chain %s to %s", base_frame.c_str(), tip_frame.c_str());
     return false;
   }
 
@@ -71,12 +59,13 @@ bool LinguaMechanicaKinematicsPlugin::initialize(const std::string &robot_descri
   joint_min.resize(num_joints_);
   joint_max.resize(num_joints_);
 
+  const urdf::ModelInterfaceSharedPtr& urdf_robot_model = robot_model_->getURDF();
   uint joint_num = 0;
   for (unsigned int i = 0; i < chain_segs.size(); ++i)
   {
 
     link_names_.push_back(chain_segs[i].getName());
-    joint = robot_model.getJoint(chain_segs[i].getJoint().getName());
+    joint = urdf_robot_model->getJoint(chain_segs[i].getJoint().getName());
     if (joint->type != urdf::Joint::UNKNOWN && joint->type != urdf::Joint::FIXED)
     {
       joint_num++;
@@ -116,14 +105,65 @@ bool LinguaMechanicaKinematicsPlugin::initialize(const std::string &robot_descri
     }
   }
 
-  ROS_INFO_NAMED("lingua mechanica plugin", "Looking in common namespaces for param name: %s", (group_name + "/position_only_ik").c_str());
+
+
+
+  ROS_INFO_NAMED("LinguaMechanicaKinematicsPlugin", "Looking in common namespaces for param name: %s", (group_name + "/position_only_ik").c_str());
   lookupParam(group_name + "/position_only_ik", position_ik_, false);
-  ROS_INFO_NAMED("lingua mechanica plugin", "Looking in common namespaces for param name: %s", (group_name + "/solve_type").c_str());
+  ROS_INFO_NAMED("LinguaMechanicaKinematicsPlugin", "Looking in common namespaces for param name: %s", (group_name + "/solve_type").c_str());
   lookupParam(group_name + "/solve_type", solve_type, std::string("Speed"));
-  ROS_INFO_NAMED("lingua mechanica plugin", "Using solve type %s", solve_type.c_str());
+  ROS_INFO_NAMED("LinguaMechanicaKinematicsPlugin", "Using solve type %s", solve_type.c_str());
+
+  ROS_INFO_STREAM_NAMED("LinguaMechanicaKinematicsPlugin", "LinguaMechanicaKinematicsPlugin initializing");
+  joint_model_group_ = robot_model_->getJointModelGroup(group_name);
+  if (!joint_model_group_)
+    return false;
+
+  // Get the dimension of the planning group
+  dimension_ = joint_model_group_->getVariableCount();
+  ROS_INFO_STREAM_NAMED("LinguaMechanicaKinematicsPlugin", "Dimension planning group '"
+                                   << group_name << "': " << dimension_
+                                   << ". Active Joints Models: " << joint_model_group_->getActiveJointModels().size()
+                                   << ". Mimic Joint Models: " << joint_model_group_->getMimicJointModels().size());
+
+  // Copy joint names
+  for (std::size_t i = 0; i < joint_model_group_->getJointModels().size(); ++i)
+  {
+    ik_group_info_.joint_names.push_back(joint_model_group_->getJointModelNames()[i]);
+  }
+
+  // Make sure all the tip links are in the link_names vector
+  for (const std::string& tip_frame : tip_frames_)
+  {
+    if (!joint_model_group_->hasLinkModel(tip_frame))
+    {
+      ROS_ERROR_NAMED("LinguaMechanicaKinematicsPlugin", "Could not find tip name '%s' in joint group '%s'", tip_frame.c_str(), group_name.c_str());
+      return false;
+    }
+    ik_group_info_.link_names.push_back(tip_frame);
+  }
+
+  std::string ik_service_name;
+  lookupParam("kinematics_solver_service_name", ik_service_name, std::string("solve_ik"));
+
+  // Setup the joint state groups that we need
+  robot_state_ = std::make_shared<moveit::core::RobotState>(robot_model_);
+  robot_state_->setToDefaultValues();
+
+  // Create the ROS service client
+  ros::NodeHandle nonprivate_handle("");
+  ik_service_client_ = std::make_shared<ros::ServiceClient>(
+      nonprivate_handle.serviceClient<lingua_mechanica_kinematics_msgs::GetPositionIK>(ik_service_name));
+  if (!ik_service_client_->waitForExistence(ros::Duration(0.1)))  // wait 0.1 seconds, blocking
+    ROS_WARN_STREAM_NAMED("LinguaMechanicaKinematicsPlugin",
+                          "Unable to connect to ROS service client with name: " << ik_service_client_->getService());
+  else
+    ROS_INFO_STREAM_NAMED("LinguaMechanicaKinematicsPlugin", "Service client started with ROS service name: " << ik_service_client_->getService());
 
   active_ = true;
-  return true;
+  ROS_DEBUG_NAMED("LinguaMechanicaKinematicsPlugin", "ROS service-based kinematics solver initialized");
+  
+  return active_;
 }
 
 
@@ -148,13 +188,13 @@ bool LinguaMechanicaKinematicsPlugin::getPositionFK(const std::vector<std::strin
 {
   if (!active_)
   {
-    ROS_ERROR_NAMED("lingua mechanica", "kinematics not active");
+    ROS_ERROR_NAMED("LinguaMechanicaKinematicsPlugin", "kinematics not active");
     return false;
   }
   poses.resize(link_names.size());
   if (joint_angles.size() != num_joints_)
   {
-    ROS_ERROR_NAMED("lingua mechanica", "Joint angles vector must have size: %d", num_joints_);
+    ROS_ERROR_NAMED("LinguaMechanicaKinematicsPlugin", "Joint angles vector must have size: %d", num_joints_);
     return false;
   }
 
@@ -173,14 +213,14 @@ bool LinguaMechanicaKinematicsPlugin::getPositionFK(const std::vector<std::strin
   bool valid = true;
   for (unsigned int i = 0; i < poses.size(); i++)
   {
-    ROS_DEBUG_NAMED("lingua mechanica", "End effector index: %d", getKDLSegmentIndex(link_names[i]));
+    ROS_DEBUG_NAMED("LinguaMechanicaKinematicsPlugin", "End effector index: %d", getKDLSegmentIndex(link_names[i]));
     if (fk_solver.JntToCart(jnt_pos_in, p_out, getKDLSegmentIndex(link_names[i])) >= 0)
     {
       tf::poseKDLToMsg(p_out, poses[i]);
     }
     else
     {
-      ROS_ERROR_NAMED("lingua mechanica", "Could not compute FK for %s", link_names[i].c_str());
+      ROS_ERROR_NAMED("LinguaMechanicaKinematicsPlugin", "Could not compute FK for %s", link_names[i].c_str());
       valid = false;
     }
   }
@@ -294,7 +334,8 @@ bool LinguaMechanicaKinematicsPlugin::searchPositionIK(const geometry_msgs::Pose
     const std::vector<double> &consistency_limits,
     const kinematics::KinematicsQueryOptions &options) const
 {
-  ROS_DEBUG_STREAM_NAMED("lingua mechanica", "getPositionIK");
+  //TODO: this is a hack, make it properly generic
+  const std::vector<geometry_msgs::Pose> ik_poses =  {ik_pose};
 
   if (!active_)
   {
@@ -305,90 +346,151 @@ bool LinguaMechanicaKinematicsPlugin::searchPositionIK(const geometry_msgs::Pose
 
   if (ik_seed_state.size() != num_joints_)
   {
-    ROS_ERROR_STREAM_NAMED("lingua mechanica", "Seed state must have size " << num_joints_ << " instead of size " << ik_seed_state.size());
+    ROS_ERROR_STREAM_NAMED("LinguaMechanicaKinematicsPlugin", "Seed state must have size " << num_joints_ << " instead of size " << ik_seed_state.size());
     error_code.val = error_code.NO_IK_SOLUTION;
     return false;
   }
-  torch::Tensor tensor = torch::tensor({ik_pose.position.x, ik_pose.position.y, ik_pose.position.z, 
-    ik_pose.orientation.x, ik_pose.orientation.y, ik_pose.orientation.z, ik_pose.orientation.w});
+  //torch::Tensor tensor = torch::tensor({ik_pose.position.x, ik_pose.position.y, ik_pose.position.z, 
+  // ik_pose.orientation.x, ik_pose.orientation.y, ik_pose.orientation.z, ik_pose.orientation.w});
 
-  //KDL::Frame frame;
-  //tf::poseMsgToKDL(ik_pose, frame);
-  //KDL::JntArray in(num_joints_), out(num_joints_);
+  lingua_mechanica_kinematics_msgs::GetPositionIK ik_srv;
+  ik_srv.request.ik_request.avoid_collisions = true;
+  ik_srv.request.ik_request.group_name = getGroupName();
 
-  //for (uint z = 0; z < num_joints_; z++)
-  //  in(z) = ik_seed_state[z];
+  // Copy seed state into virtual robot state and convert into moveit_msg
+  robot_state_->setJointGroupPositions(joint_model_group_, ik_seed_state);
+  moveit::core::robotStateToRobotStateMsg(*robot_state_, ik_srv.request.ik_request.robot_state);
 
-  //KDL::Twist bounds = KDL::Twist::Zero();
+  // Load the poses into the request in difference places depending if there is more than one or not
+  geometry_msgs::PoseStamped ik_pose_st;
+  ik_pose_st.header.frame_id = base_frame_;
+  if (tip_frames_.size() > 1)
+  {
+    // Load into vector of poses
+    for (std::size_t i = 0; i < tip_frames_.size(); ++i)
+    {
+      ik_pose_st.pose = ik_poses[i];
+      ik_srv.request.ik_request.pose_stamped_vector.push_back(ik_pose_st);
+      ik_srv.request.ik_request.ik_link_names.push_back(tip_frames_[i]);
+    }
+  }
+  else
+  {
+    ik_pose_st.pose = ik_poses[0];
 
-  //if (position_ik_)
-  //{
-  //  bounds.rot.x(std::numeric_limits<float>::max());
-  //  bounds.rot.y(std::numeric_limits<float>::max());
-  //  bounds.rot.z(std::numeric_limits<float>::max());
-  //}
-  
-  //double epsilon = 1e-5;  //Same as MoveIt's KDL plugin
+    // Load into single pose value
+    ik_srv.request.ik_request.pose_stamped = ik_pose_st;
+    ik_srv.request.ik_request.ik_link_name = getTipFrames()[0];
+  }
 
-  //lingua mechanica::SolveType solvetype;
 
-  //if (solve_type == "Manipulation1")
-  //  solvetype = lingua mechanica::Manip1;
-  //else if (solve_type == "Manipulation2")
-  //  solvetype = lingua mechanica::Manip2;
-  //else if (solve_type == "Distance")
-  //  solvetype = lingua mechanica::Distance;
-  //else
-  // {
-  //  if (solve_type != "Speed")
-  //  {
-  //    ROS_WARN_STREAM_NAMED("lingua mechanica", solve_type << " is not a valid solve_type; setting to default: Speed");
-  //  }
-  //  solvetype = lingua mechanica::Speed;
-  //}
+  ROS_DEBUG_STREAM_NAMED("LinguaMechanicaKinematicsPlugin", "Calling service: " << ik_service_client_->getService());
+  if (ik_service_client_->call(ik_srv))
+  {
+    // Check error code
+    error_code.val = ik_srv.response.error_code.val;
+    if (error_code.val != error_code.SUCCESS)
+    {
+      // TODO: check all solutions
+      ROS_DEBUG_STREAM_NAMED("LinguaMechanicaKinematicsPlugin", "An IK that satisifes the constraints and is collision free could not be found."
+                                        << "\nRequest was: \n"
+                                        << ik_srv.request.ik_request << "\nResponse was: \n"
+                                        << ik_srv.response.solutions.at(0));
+      switch (error_code.val)
+      {
+        case moveit_msgs::MoveItErrorCodes::FAILURE:
+          ROS_ERROR_STREAM_NAMED("LinguaMechanicaKinematicsPlugin", "Service failed with with error code: FAILURE");
+          break;
+        case moveit_msgs::MoveItErrorCodes::NO_IK_SOLUTION:
+          ROS_ERROR_STREAM_NAMED("LinguaMechanicaKinematicsPlugin", "Service failed with with error code: NO IK SOLUTION");
+          break;
+        default:
+          ROS_ERROR_STREAM_NAMED("LinguaMechanicaKinematicsPlugin", "Service failed with with error code: " << error_code.val);
+      }
+      return false;
+    }
+  }
+  else
+  {
+    ROS_ERROR_STREAM("Service call failed to connect to service: " << ik_service_client_->getService());
+    error_code.val = error_code.FAILURE;
+    return false;
+  }
+  // TODO: use arrays
+  // Convert the robot state message to our robot_state representation
+  // TODO: check all solutions
+  if (!moveit::core::robotStateMsgToRobotState(ik_srv.response.solutions[0], *robot_state_))
+  {
+    ROS_ERROR_STREAM_NAMED("LinguaMechanicaKinematicsPlugin",
+                           "An error occured converting received robot state message into internal robot state.");
+    error_code.val = error_code.FAILURE;
+    return false;
+  }
 
-  //lingua mechanica::lingua mechanica ik_solver(chain, joint_min, joint_max, timeout, epsilon, solvetype);
-  //int rc = ik_solver.CartToJnt(in, frame, out, bounds);
+  // Get just the joints we are concerned about in our planning group
+  robot_state_->copyJointGroupPositions(joint_model_group_, solution);
 
-  // TODO: placeholder for inverse kinematics
+  //TODO: callback for all the poses as a filter
+  // Run the solution callback (i.e. collision checker) if available
+  if (!solution_callback.empty())
+  {
+    ROS_DEBUG_STREAM_NAMED("LinguaMechanicaKinematicsPlugin", "Calling solution callback on IK solution");
 
-  //solution.resize(num_joints_);
+    // hack: should use all poses, not just the 0th
+    solution_callback(ik_poses[0], solution, error_code);
 
-  //if (rc >= 0)
-  //{
-  //  for (uint z = 0; z < num_joints_; z++)
-  //    solution[z] = out(z);
+    if (error_code.val != error_code.SUCCESS)
+    {
+      switch (error_code.val)
+      {
+        case moveit_msgs::MoveItErrorCodes::FAILURE:
+          ROS_ERROR_STREAM_NAMED("LinguaMechanicaKinematicsPlugin", "IK solution callback failed with with error code: FAILURE");
+          break;
+        case moveit_msgs::MoveItErrorCodes::NO_IK_SOLUTION:
+          ROS_ERROR_STREAM_NAMED("LinguaMechanicaKinematicsPlugin", "IK solution callback failed with with error code: "
+                                        "NO IK SOLUTION");
+          break;
+        default:
+          ROS_ERROR_STREAM_NAMED("LinguaMechanicaKinematicsPlugin", "IK solution callback failed with with error code: " << error_code.val);
+      }
+      return false;
+    }
+  }
 
-    // check for collisions if a callback is provided
-  //  if (!solution_callback.empty())
-  //  {
-  //    solution_callback(ik_pose, solution, error_code);
-  //    if (error_code.val == moveit_msgs::MoveItErrorCodes::SUCCESS)
-  //    {
-  //      ROS_DEBUG_STREAM_NAMED("lingua mechanica", "Solution passes callback");
-  //      return true;
-  //    }
-  //    else
-  //    {
-  //      ROS_DEBUG_STREAM_NAMED("lingua mechanica", "Solution has error code " << error_code);
-  //      return false;
-  //    }
-  //  }
-  //  else
-  //    return true; // no collision check callback provided
-  //}
-
-  //error_code.val = moveit_msgs::MoveItErrorCodes::NO_IK_SOLUTION;
-  solution.push_back(0.1);
-  solution.push_back(0.2);
-  solution.push_back(0.3);
-  solution.push_back(0.4);
-  solution.push_back(0.5);
-  solution.push_back(0.6);
-  error_code.val = moveit_msgs::MoveItErrorCodes::SUCCESS;
+  ROS_INFO_STREAM_NAMED("LinguaMechanicaKinematicsPlugin", "IK Solver Succeeded!");
   return true;
 }
 
+
+
+/**
+ * @brief Given the desired poses of all end-effectors, compute joint angles that are able to reach it.
+ *
+ * The default implementation returns only one solution and so its result is equivalent to calling
+ * 'getPositionIK(...)' with a zero initialized seed.
+ *
+ * Some planners (e.g. IKFast) support getting multiple joint solutions for a single pose.
+ * This can be enabled using the |DiscretizationMethods| enum and choosing an option that is not |NO_DISCRETIZATION|.
+ *
+ * @param ik_poses  The desired pose of each tip link
+ * @param ik_seed_state an initial guess solution for the inverse kinematics
+ * @param solutions A vector of valid joint vectors. This return has two variant behaviors:
+ *                  1) Return a joint solution for every input |ik_poses|, e.g. multi-arm support
+ *                  2) Return multiple joint solutions for a single |ik_poses| input, e.g. underconstrained IK
+ *                  TODO(dave): This dual behavior is confusing and should be changed in a future refactor of this API
+ * @param result A struct that reports the results of the query
+ * @param options An option struct which contains the type of redundancy discretization used. This default
+ *                implementation only supports the KinematicSearches::NO_DISCRETIZATION method; requesting any
+ *                other will result in failure.
+ * @return True if a valid set of solutions was found, false otherwise.
+ */
+bool LinguaMechanicaKinematicsPlugin::getPositionIK(const std::vector<geometry_msgs::Pose>& ik_poses, const std::vector<double>& ik_seed_state,
+                            std::vector<std::vector<double> >& solutions, kinematics::KinematicsResult& result,
+                            const kinematics::KinematicsQueryOptions& options) const {
+
+  //TODO: implement
+  return false;
+}
 
 
 } // end namespace
